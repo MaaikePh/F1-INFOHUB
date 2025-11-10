2d1a52f552ca8067b0675db1ac9d7d55dd751380
MaaikePh <maaikephielix@live.nl>
2025-11-10 16:23:10 +0100

    Toegevoegd: React Router en basisroutes in main.jsx en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home/Homepage.jsx'
+import Racekalender from './pages/Racekalender/Racekalender.jsx';
+import Dashboard from './pages/Dashboard/Dashboard.jsx';
+import Inloggen from './pages/Authenticatie/Authenticatie.jsx';
+import Detailpage from './pages/CoureurDetail/CoureurDetail.jsx';
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/racekalender" element={<Racekalender />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/inloggen" element={<Inloggen />} />
+        <Route path="/coureur/:id" element={<Detailpage />} />
+      </Routes>
     </>
   )
 }
