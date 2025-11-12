977790e05aaa2dcf95ab66ebfa9d476487ea5c7a
MaaikePh <maaikephielix@live.nl>
2025-11-12 15:25:50 +0100

    Toegevoegd: Basis-structuur en titel

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,8 +1,18 @@
 import './Homepage.css';
+import testdata from '/src/constants/test-api-data.json';
 
 function Homepage() {
     return (
-        <></>
+        <main>
+            <section>
+
+            </section>
+            <section>
+                <h1 className="title">
+                    F1 coureurs {testdata.season}
+                </h1>
+            </section>
+        </main>
     )
 }
 
