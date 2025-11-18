523abf2caa0c3ab0122e18f9ce87f2a80432c625
MaaikePh <maaikephielix@live.nl>
2025-11-18 15:27:06 +0100

    Toegevoegd: RaceFilter component voorzien van styling

--- src/components/RaceFilter/RaceFilter.jsx
+++ src/components/RaceFilter/RaceFilter.jsx
@@ -1,8 +1,45 @@
 import './RaceFilter.css';
+import {useForm} from 'react-hook-form';
 
-function RaceFilter(props) {
+function RaceFilter({onFilterChange}) {
+    const {register, watch} = useForm({
+        defaultValues: {
+        month: 'all'
+        }
+    });
+
+    const selectedMonth = watch('month');
+
+    function handleMonthChange() {
+        onFilterChange(selectedMonth);
+    }
+
     return (
-        <></>
+        <form className='race-filter-form'>
+            <label htmlFor='month-filter' className='race-filter-label'>
+                Filter op maand
+            </label>
+
+                <select
+                    onChange={handleMonthChange}
+                    id='month-filter'
+                    {...register('month')}
+                    className='race-filter-select'
+                    >
+
+                    <option value="all">Kies maand</option>
+                    <option value="march">Maart</option>
+                    <option value="april">April</option>
+                    <option value="may">Mei</option>
+                    <option value="june">Juni</option>
+                    <option value="july">Juli</option>
+                    <option value="august">Augustus</option>
+                    <option value="september">September</option>
+                    <option value="october">Oktober</option>
+                    <option value="november">November</option>
+                    <option value="december">December</option>
+                </select>
+        </form>
     )
 }
 
--- src/pages/Racekalender/Racekalender.jsx
+++ src/pages/Racekalender/Racekalender.jsx
@@ -2,16 +2,17 @@ import './Racekalender.css';
 import testdata from '/src/constants/test-api-data.json';
 import RaceFilter from '../../components/RaceFilter/RaceFilter.jsx';
 import RaceCard from '../../components/RaceCard/RaceCard.jsx';
+import {useState} from 'react';
 
 function Racekalender() {
+    const [monthFilter, setMonthFilter] = useState('all');
+
     return (
         <main>
             <h1 className='title'>Racekalender {testdata.season}</h1>
 
             <section className='race-filter-container'>
-                <form>
-                    <RaceFilter />
-                </form>
+                    <RaceFilter onFilterChange={setMonthFilter} />
             </section>
 
             <section className='all-races-container'>
